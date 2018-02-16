75aade9e9652ea5c95f1488c982d92fb01baa2fb
Ranjanjjbytes <ranjan.jjbyte@gmail.com>
2018-02-16 17:34:04 +0530

    new updates

--- movilo/controller/user_controller.php
+++ movilo/controller/user_controller.php
@@ -70,7 +70,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: '.$email. "\r\n";
+$headers .= 'From: '.$email."\r\n";
 
 	   if(mail($to, $subject, $message,$headers)){
 		   echo '<script>alert("Message sent successfully !")</script>';	
