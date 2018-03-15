ec6c29c74a383af3e7ab3f6dcb6dd874737113de
Ranjanjjbytes <ranjan.jjbyte@gmail.com>
2018-03-15 18:27:03 +0530

    new updates

--- blogs/wp-config.php
+++ blogs/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'yapnaa-blogs');
+define('DB_NAME', 'yapnaa_blogs');
 
 /** MySQL database username */
-define('DB_USER', 'yapnaa-blogs');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'yusuf@Bl0g$');
+define('DB_PASSWORD', 'M0v!Lo@987');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
